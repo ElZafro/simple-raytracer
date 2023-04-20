7f3a953a7547bed8a0d78440ef002289c58365f8
Jaime Zafra Ruiz <jaimezr27@gmail.com>
2023-04-20 22:14:29 +0200

    added normals

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use image::{ImageBuffer, Rgb};
 use nalgebra::Vector3;
 use ray::Ray;
 
-fn hit_sphere(r: &Ray) -> bool {
+fn hit_sphere(r: &Ray) -> Option<f64> {
     
     let (center, radius) = (Vector3::new(0.0,0.0,-1.0), 0.5);
 
-    let oc = center - r.origin;
+    let oc = r.origin - center;
     let a = r.direction.norm_squared();
     let b = 2.0 * oc.dot(&r.direction);
     let c = oc.norm_squared() - radius * radius;
 
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    
+    if discriminant <= 0.0  { return None; }
+    Some((- b - discriminant.sqrt()) / (2.0 * a))
 }
 
 fn ray_color(r: &Ray) -> Vector3<f64> {
 
-    if hit_sphere(r) {
-        return Vector3::new(0.9,0.5,0.8);
+    if let Some(t) = hit_sphere(r) {
+        let n = (r.at(t) - Vector3::new(0.0, 0.0, -1.0)).normalize();
+        return 0.5 * Vector3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
     }
 
     let unit_direction = r.direction.normalize();
