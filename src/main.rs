ae218ced08b9360cff455db568aaa886ae09f063
Jaime Zafra Ruiz <jaimezr27@gmail.com>
2023-04-23 21:32:40 +0200

    Added lambertian scattering

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,32 @@ mod sphere;
 use hit::{Hit, World};
 use image::{ImageBuffer, Rgb};
 use nalgebra::Vector3;
+use rand::random;
 use ray::Ray;
 use sphere::Sphere;
 
-fn ray_color(r: &Ray, world: &World) -> Vector3<f64> {
+fn ray_color(r: &Ray, world: &World, depth: u64) -> Vector3<f64> {
 
-    if let Some(hit_record) = world.hit(r, 0.0, f64::MAX) {
-        let n = hit_record.normal;
-        return 0.5 * Vector3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+    if depth == 0 { return Vector3::new(0.0, 0.0, 0.0); }
+
+    if let Some(hit_record) = world.hit(r, 0.001, f64::MAX) {
+        let random_normalized_vector = Vector3::new(random::<f64>() * 2.0 - 1.0 , random::<f64>() * 2.0 - 1.0, random::<f64>() * 2.0 - 1.0).normalize();
+        let target = hit_record.point + hit_record.normal + random_normalized_vector;
+        let ray = Ray::new(hit_record.point, target);
+        return 0.5 * ray_color(&ray, world, depth - 1);
     }
 
     let unit_direction = r.direction.normalize();
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
+}
+
+fn get_color(pixel_color: Vector3<f64>) -> Rgb<u8> {
+    Rgb([
+        (256.0 * (pixel_color.x).sqrt().clamp(0.0, 0.999)) as u8,
+        (256.0 * (pixel_color.y).sqrt().clamp(0.0, 0.999)) as u8,
+        (256.0 * (pixel_color.z).sqrt().clamp(0.0, 0.999)) as u8,
+    ])
 }
 
 fn main() {
@@ -26,6 +39,8 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const WIDTH: usize = 256;
     const HEIGHT: usize = ((256_f64) / ASPECT_RATIO) as usize;
+    const SAMPLES_PER_PIXEL: usize = 100;
+    const MAX_DEPTH: u64 = 5;
 
     //World
     let mut world = World::new();
@@ -36,7 +51,6 @@ fn main() {
     let viewport_height = 2.0;
     let viewport_width = viewport_height * ASPECT_RATIO;
     let focal_length = 1.0;
-    const SAMPLES_PER_PIXEL: usize = 100;
 
     let origin = Vector3::new(0.0, 0.0, 0.0);
     let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
@@ -58,10 +72,10 @@ fn main() {
 
             let direction = lower_left_corner + u * horizontal + v * vertical - origin;
             let ray = Ray::new(origin, direction);
-            pixel_color += ray_color(&ray, &world);
+            pixel_color += ray_color(&ray, &world, MAX_DEPTH);
         }
         pixel_color /= SAMPLES_PER_PIXEL as f64;
-        *pixel = Rgb([(pixel_color.x * 255.999) as u8, (pixel_color.y * 255.999) as u8, (pixel_color.z * 255.999) as u8]);
+        *pixel = get_color(pixel_color);
     }
 
     imgbuf.save("image.png").unwrap();
